dcbdaccdcaad44f0c496ae8996ab89068a8a791c
Ulrich R <44271004+UlricusR@users.noreply.github.com>
2026-01-03 18:03:26 +0100

    Added selector for home consumptions in charging cost plan editor

---
+++ CleverCheck/Views/HomeConsumptionPicker.swift
@@ -0,0 +1,18 @@
+//
+//  HomeConsumptionPicker.swift
+//  CleverCheck
+//
+//  Created by Ulrich Rüth on 03/01/2026.
+//
+
+import SwiftUI
+
+struct HomeConsumptionPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeConsumptionPicker()
+}
