9b179d37f761b443a1cb48bd2e22bbd8bd883e2f
Ulrich R <44271004+UlricusR@users.noreply.github.com>
2026-05-15 14:23:38 +0200

    Enabling selection of refunded charging sessions

--- CleverCheck/Views/ChargingSessionPicker.swift
+++ CleverCheck/Views/ChargingSessionPicker.swift
@@ -27,92 +27,86 @@ struct ChargingSessionPicker: View {
                 .font(.headline)
                 .padding()
             
-            if !showRefundedSessions {
-                Button(action: {
-                    let sessionsToSave = Array(selectedSessions)
-                    let sessionsToRemove = Set(possibleSessions).subtracting(selectedSessions)
-                    
-                    for session in sessionsToSave {
-                        session.relatedHomeConsumption = homeConsumption
-                    }
-                    
-                    for session in sessionsToRemove {
-                        // If the session is assigned to another home consumption, leave it there, otherwise set to nil
-                        if let relatedHomeConsumption = session.relatedHomeConsumption {
-                            if relatedHomeConsumption.id == homeConsumption.id {
-                                session.relatedHomeConsumption = nil
-                            }
+            Button(action: {
+                let sessionsToSave = Array(selectedSessions)
+                let sessionsToRemove = Set(possibleSessions).subtracting(selectedSessions)
+                
+                for session in sessionsToSave {
+                    session.relatedHomeConsumption = homeConsumption
+                }
+                
+                for session in sessionsToRemove {
+                    // If the session is assigned to another home consumption, leave it there, otherwise set to nil
+                    if let relatedHomeConsumption = session.relatedHomeConsumption {
+                        if relatedHomeConsumption.id == homeConsumption.id {
+                            session.relatedHomeConsumption = nil
                         }
                     }
-                    
-                    try? modelContext.save()
-                    
-                    // Close the sheet
-                    isShowing = false
-                }) {
-                    Text("Save")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .font(.system(size: 18))
-                        .padding()
-                        .foregroundColor(.white)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.white, lineWidth: 2)
-                        )
                 }
-                .background(Color.blue)
-                .cornerRadius(25)
-                .padding()
                 
-                Toggle("Ignore consumption type", isOn: $ignoreConsumptionType)
-                    .padding(.horizontal)
-                Divider()
+                try? modelContext.save()
                 
-                Button(action: {
-                    selectAll.toggle()
-                    if selectAll {
-                        selectedSessions = Set(possibleSessions)
-                    } else {
-                        selectedSessions.removeAll()
-                    }
-                }) {
-                    Image(systemName: selectAll ? "xmark.square" : "square")
-                        .imageScale(.large)
-                        .foregroundStyle(selectAll ? .green : .gray)
-                    Text("Select all")
-                        .foregroundStyle(.primary)
-                    Spacer()
-                }
-                .padding()
+                // Close the sheet
+                isShowing = false
+            }) {
+                Text("Save")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .font(.system(size: 18))
+                    .padding()
+                    .foregroundColor(.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 2)
+                    )
             }
+            .background(Color.blue)
+            .cornerRadius(25)
+            .padding()
+            
+            Toggle("Ignore consumption type", isOn: $ignoreConsumptionType)
+                .padding(.horizontal)
+            Divider()
+            
+            Button(action: {
+                selectAll.toggle()
+                if selectAll {
+                    selectedSessions = Set(possibleSessions)
+                } else {
+                    selectedSessions.removeAll()
+                }
+            }) {
+                Image(systemName: selectAll ? "xmark.square" : "square")
+                    .imageScale(.large)
+                    .foregroundStyle(selectAll ? .green : .gray)
+                Text("Select all")
+                    .foregroundStyle(.primary)
+                Spacer()
+            }
+            .padding()
             
             List(possibleSessions, id: \.id) { session in
                 HStack {
-                    if !showRefundedSessions {
-                        Image(systemName: selectedSessions.contains(session) ? "xmark.square" : "square")
-                            .imageScale(.large)
-                            .foregroundStyle(selectedSessions.contains(session) ? .green : .gray)
-                    }
+                    Image(systemName: selectedSessions.contains(session) ? "xmark.square" : "square")
+                        .imageScale(.large)
+                        .foregroundStyle(selectedSessions.contains(session) ? .green : .gray)
                     Text(session.description)
                 }
                 .onTapGesture {
-                    if !showRefundedSessions {
-                        if selectedSessions.contains(session) {
-                            selectedSessions.remove(session)
-                        } else {
-                            selectedSessions.insert(session)
-                        }
-                        if selectedSessions.count == possibleSessions.count {
-                            selectAll = true
-                        } else {
-                            selectAll = false
-                        }
+                    if selectedSessions.contains(session) {
+                        selectedSessions.remove(session)
+                    } else {
+                        selectedSessions.insert(session)
+                    }
+                    if selectedSessions.count == possibleSessions.count {
+                        selectAll = true
+                    } else {
+                        selectAll = false
                     }
                 }
             }
         }
         .onAppear {
-            if let preselectedSessions = homeConsumption.chargingSessions {
+            if let preselectedSessions = showRefundedSessions ? homeConsumption.refundedChargingSessions : homeConsumption.chargingSessions {
                 let possibleSessions = Set(possibleSessions)
                 let commonSessions = possibleSessions.intersection(preselectedSessions)
                 selectedSessions = commonSessions
